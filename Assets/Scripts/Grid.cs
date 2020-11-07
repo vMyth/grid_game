dc8b87694f9468760208345337ec5ecb086d774f
Vishal Pandey <vishalp055@gmail.com>
2020-11-07 15:54:33 +0530

    Add Unity Project

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Grid : MonoBehaviour
+{
+	public Tile tilePrefab;
+	public int numberOfTiles = 10;
+	public float distanceBetweenTiles = 1f;
+	public int tilesPerRow = 4;
+	public int numberOfTarget = 5;
+
+	public static Tile[] tilesAll;
+	public static ArrayList targetTiles;
+	public static ArrayList nonTargetTiles;
+
+	public static int clickCount = 0;
+	public static int score = 0;
+
+	void Start()
+	{
+		CreateTiles();
+	}
+
+    private void Update()
+    {
+        if(clickCount == 20)
+        {
+			if(score == 10)
+            {
+				Debug.Log("Win");
+            }
+            else
+            {
+				Debug.Log("Lose");
+            }
+        }
+    }
+
+    private void CreateTiles()
+    {
+		tilesAll = new Tile[numberOfTiles];
+
+		float xOffset = 0f;
+		float zOffset = 0f;
+
+		for (int tilesCreated = 0; tilesCreated < numberOfTiles; tilesCreated++)
+		{
+			xOffset += distanceBetweenTiles;
+
+			if (tilesCreated % tilesPerRow == 0)
+			{
+				zOffset += distanceBetweenTiles;
+				xOffset = 0;
+			}
+
+			Tile newTile = (Tile)Instantiate(tilePrefab, new Vector3(transform.position.x + xOffset, transform.position.y, transform.position.z + zOffset), transform.rotation);
+			newTile.ID = tilesCreated;
+			newTile.tilesPerRow = tilesPerRow;
+			tilesAll[tilesCreated] = newTile;
+		}
+
+		AssignTarget();
+	}
+
+    private void AssignTarget()
+    {
+		nonTargetTiles = new ArrayList(tilesAll);
+		targetTiles = new ArrayList();
+
+		for (int targetAssigned = 0; targetAssigned < numberOfTarget; targetAssigned++)
+		{
+
+			Tile currentTile = (Tile)nonTargetTiles[UnityEngine.Random.Range(0, nonTargetTiles.Count)];
+
+			currentTile.GetComponent<Tile>().isTarget = true;
+
+			//Add to Tiles mined
+			targetTiles.Add(currentTile);
+			//Remove from unmined
+			nonTargetTiles.Remove(currentTile);
+		}
+	}
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System;
+
+public class Tile : MonoBehaviour
+{
+    public bool isTarget = false;
+    public TextMesh displayText;
+    public Material materialIdle;
+    public Material materialLightup;
+    public Material materialRed;
+    public Material materialYellow;
+    public Material materialGreen;
+
+    public int ID;
+    public int tilesPerRow;
+
+    public Tile tileUpper;
+    public Tile tileLower;
+    public Tile tileLeft;
+    public Tile tileRight;
+    
+    public Tile tileUpperS;
+    public Tile tileLowerS;
+    public Tile tileLeftS;
+    public Tile tileRightS;
+
+    public Tile tileUpperLeft;
+    public Tile tileUpperRight;
+    public Tile tileLowerLeft;
+    public Tile tileLowerRight;
+    
+    public Tile tileUpperLeftS;
+    public Tile tileUpperRightS;
+    public Tile tileLowerLeftS;
+    public Tile tileLowerRightS;
+    
+    public Tile tileUpperLeftS1;
+    public Tile tileUpperLeftS2;
+    public Tile tileUpperRightS1;
+    public Tile tileUpperRightS2;
+    public Tile tileLowerLeftS1;
+    public Tile tileLowerLeftS2;
+    public Tile tileLowerRightS1;
+    public Tile tileLowerRightS2;
+
+    public List<Tile> adjacentTiles = new List<Tile>();
+    public int adjacentTarget = 0;
+
+    public string state = "idle";
+
+    private void Start()
+    {
+        gameObject.name = "Tile " + ID.ToString();
+
+        if (inBounds(Grid.tilesAll, ID + tilesPerRow)) { tileUpper = Grid.tilesAll[ID + tilesPerRow]; }
+        if (inBounds(Grid.tilesAll, ID - tilesPerRow)) { tileLower = Grid.tilesAll[ID - tilesPerRow]; }
+        if (inBounds(Grid.tilesAll, ID + 1) && (ID + 1) % tilesPerRow != 0) { tileRight = Grid.tilesAll[ID + 1]; }
+        if (inBounds(Grid.tilesAll, ID - 1) && ID % tilesPerRow != 0) { tileLeft = Grid.tilesAll[ID - 1]; }
+        
+        if (inBounds(Grid.tilesAll, ID + (2 * tilesPerRow))) { tileUpperS = Grid.tilesAll[ID + (2 * tilesPerRow)]; }
+        if (inBounds(Grid.tilesAll, ID - (2 * tilesPerRow))) { tileLowerS = Grid.tilesAll[ID - (2 * tilesPerRow)]; }
+        if (inBounds(Grid.tilesAll, ID + 2) && (ID + 2) % tilesPerRow != 0) { tileRightS = Grid.tilesAll[ID + 2]; }
+        if (inBounds(Grid.tilesAll, ID - 2) && ID % tilesPerRow != 0) { tileLeftS = Grid.tilesAll[ID - 2]; }
+
+        if (inBounds(Grid.tilesAll, ID + tilesPerRow + 1) && (ID + tilesPerRow + 1) % tilesPerRow != 0) { tileUpperRight = Grid.tilesAll[ID + tilesPerRow + 1]; }
+        if (inBounds(Grid.tilesAll, ID + tilesPerRow - 1) && ID % tilesPerRow != 0) { tileUpperLeft = Grid.tilesAll[ID + tilesPerRow - 1]; }
+        if (inBounds(Grid.tilesAll, ID - tilesPerRow + 1) && (ID + 1) % tilesPerRow != 0) { tileLowerRight = Grid.tilesAll[ID - tilesPerRow + 1]; }
+        if (inBounds(Grid.tilesAll, ID - tilesPerRow - 1) && ID % tilesPerRow != 0) { tileLowerLeft = Grid.tilesAll[ID - tilesPerRow - 1]; }
+        
+        if (inBounds(Grid.tilesAll, ID + tilesPerRow + 2) && (ID + tilesPerRow + 2) % tilesPerRow != 0) { tileUpperRightS = Grid.tilesAll[ID + tilesPerRow + 2]; }
+        if (inBounds(Grid.tilesAll, ID + tilesPerRow - 2) && ID % tilesPerRow != 0) { tileUpperLeftS = Grid.tilesAll[ID + tilesPerRow - 2]; }
+        if (inBounds(Grid.tilesAll, ID - tilesPerRow + 2) && (ID + 2) % tilesPerRow != 0) { tileLowerRightS = Grid.tilesAll[ID - tilesPerRow + 2]; }
+        if (inBounds(Grid.tilesAll, ID - tilesPerRow - 2) && ID % tilesPerRow != 0) { tileLowerLeftS = Grid.tilesAll[ID - tilesPerRow - 2]; }
+
+        if (inBounds(Grid.tilesAll, ID + (2 * tilesPerRow) + 1) && (ID + (2 * tilesPerRow) + 1) % tilesPerRow != 0) { tileUpperRightS1 = Grid.tilesAll[ID + (2 * tilesPerRow) + 1]; }
+        if (inBounds(Grid.tilesAll, ID + (2 * tilesPerRow) + 2) && (ID + (2 * tilesPerRow) + 2) % tilesPerRow != 0) { tileUpperRightS2 = Grid.tilesAll[ID + (2 * tilesPerRow) + 2]; }
+        if (inBounds(Grid.tilesAll, ID + (2 * tilesPerRow) - 1) && ID % tilesPerRow != 0) { tileUpperLeftS1 = Grid.tilesAll[ID + (2 * tilesPerRow) - 1]; }
+        if (inBounds(Grid.tilesAll, ID + (2 * tilesPerRow) - 2) && ID % tilesPerRow != 0) { tileUpperLeftS2 = Grid.tilesAll[ID + (2 * tilesPerRow) - 2]; }
+        if (inBounds(Grid.tilesAll, ID - (2 * tilesPerRow) + 1) && (ID + 1) % tilesPerRow != 0) { tileLowerRightS1 = Grid.tilesAll[ID - (2 * tilesPerRow) + 1]; }
+        if (inBounds(Grid.tilesAll, ID - (2 * tilesPerRow) + 2) && (ID + 2) % tilesPerRow != 0) { tileLowerRightS2 = Grid.tilesAll[ID - (2 * tilesPerRow) + 2]; }
+        if (inBounds(Grid.tilesAll, ID - (2 * tilesPerRow) - 1) && ID % tilesPerRow != 0) { tileLowerLeftS1 = Grid.tilesAll[ID - (2 * tilesPerRow) - 1]; }
+        if (inBounds(Grid.tilesAll, ID - (2 * tilesPerRow) - 2) && ID % tilesPerRow != 0) { tileLowerLeftS2 = Grid.tilesAll[ID - (2 * tilesPerRow) - 2]; }
+
+        if (tileUpper) { adjacentTiles.Add(tileUpper); }
+        if (tileLower) { adjacentTiles.Add(tileLower); }
+        if (tileLeft) { adjacentTiles.Add(tileLeft); }
+        if (tileRight) { adjacentTiles.Add(tileRight); }
+        
+        if (tileUpperS) { adjacentTiles.Add(tileUpperS); }
+        if (tileLowerS) { adjacentTiles.Add(tileLowerS); }
+        if (tileLeftS) { adjacentTiles.Add(tileLeftS); }
+        if (tileRightS) { adjacentTiles.Add(tileRightS); }
+
+        if (tileUpperLeft) { adjacentTiles.Add(tileUpperLeft); }
+        if (tileUpperRight) { adjacentTiles.Add(tileUpperRight); }
+        if (tileLowerLeft) { adjacentTiles.Add(tileLowerLeft); }
+        if (tileLowerRight) { adjacentTiles.Add(tileLowerRight); }
+        
+        if (tileUpperLeftS) { adjacentTiles.Add(tileUpperLeftS); }
+        if (tileUpperRightS) { adjacentTiles.Add(tileUpperRightS); }
+        if (tileLowerLeftS) { adjacentTiles.Add(tileLowerLeftS); }
+        if (tileLowerRightS) { adjacentTiles.Add(tileLowerRightS); }
+        
+        if (tileUpperLeftS1) { adjacentTiles.Add(tileUpperLeftS1); }
+        if (tileUpperLeftS2) { adjacentTiles.Add(tileUpperLeftS2); }
+        if (tileUpperRightS1) { adjacentTiles.Add(tileUpperRightS1); }
+        if (tileUpperRightS2) { adjacentTiles.Add(tileUpperRightS2); }
+        if (tileLowerLeftS1) { adjacentTiles.Add(tileLowerLeftS1); }
+        if (tileLowerLeftS2) { adjacentTiles.Add(tileLowerLeftS2); }
+        if (tileLowerRightS1) { adjacentTiles.Add(tileLowerRightS1); }
+        if (tileLowerRightS2) { adjacentTiles.Add(tileLowerRightS2); }
+
+        CountTarget();
+    }
+
+    private void CountTarget()
+    {
+        foreach (Tile currentTile in adjacentTiles)
+        {
+            if (currentTile.isTarget)
+            {
+                adjacentTarget += 1;
+            }
+        }
+
+        displayText.text = adjacentTarget.ToString();
+
+        if (adjacentTarget <= 0)
+        {
+            displayText.text = "";
+        }
+    }
+
+    private bool inBounds(Tile[] inputArray, int targetID)
+    {
+        if (targetID < 0 || targetID >= inputArray.Length)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    void OnMouseOver()
+    {
+        if (state == "idle")
+        {
+            this.GetComponent<Renderer>().material = materialLightup;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(state == "idle")
+            {
+                ++Grid.clickCount;
+
+                if (isTarget)
+                {
+                    ++Grid.score;
+                    this.GetComponent<Renderer>().material = materialRed;
+                }
+                else if(adjacentTarget > 0)
+                {
+                    this.GetComponent<Renderer>().material = materialYellow;
+                }
+                else
+                {
+                    this.GetComponent<Renderer>().material = materialGreen;
+                }
+            }
+            state = "uncovered";
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (state == "idle")
+        {
+            GetComponent<Renderer>().material = materialIdle;
+        }
+    }
+}
