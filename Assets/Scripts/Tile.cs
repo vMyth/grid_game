f5f1dd4295e993b77fe669f7a66069e8a1c02bda
Vishal Pandey <vishalp055@gmail.com>
2020-11-19 22:38:15 +0530

    Update tiles
    Commented out the part making distance from target as 2. Now only tiles as distance 1 will turn green.

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -22,17 +22,17 @@ public class Tile : MonoBehaviour
     public Tile tileLeft;
     public Tile tileRight;
     
-    public Tile tileUpperS;
+/*    public Tile tileUpperS;
     public Tile tileLowerS;
     public Tile tileLeftS;
-    public Tile tileRightS;
+    public Tile tileRightS;*/
 
     public Tile tileUpperLeft;
     public Tile tileUpperRight;
     public Tile tileLowerLeft;
     public Tile tileLowerRight;
     
-    public Tile tileUpperLeftS;
+/*    public Tile tileUpperLeftS;
     public Tile tileUpperRightS;
     public Tile tileLowerLeftS;
     public Tile tileLowerRightS;
@@ -44,7 +44,7 @@ public class Tile : MonoBehaviour
     public Tile tileLowerLeftS1;
     public Tile tileLowerLeftS2;
     public Tile tileLowerRightS1;
-    public Tile tileLowerRightS2;
+    public Tile tileLowerRightS2;*/
 
     public List<Tile> adjacentTiles = new List<Tile>();
     public int adjacentTarget = 0;
@@ -60,58 +60,58 @@ public class Tile : MonoBehaviour
         if (inBounds(Grid.tilesAll, ID + 1) && (ID + 1) % tilesPerRow != 0) { tileRight = Grid.tilesAll[ID + 1]; }
         if (inBounds(Grid.tilesAll, ID - 1) && ID % tilesPerRow != 0) { tileLeft = Grid.tilesAll[ID - 1]; }
         
-        if (inBounds(Grid.tilesAll, ID + (2 * tilesPerRow))) { tileUpperS = Grid.tilesAll[ID + (2 * tilesPerRow)]; }
+/*        if (inBounds(Grid.tilesAll, ID + (2 * tilesPerRow))) { tileUpperS = Grid.tilesAll[ID + (2 * tilesPerRow)]; }
         if (inBounds(Grid.tilesAll, ID - (2 * tilesPerRow))) { tileLowerS = Grid.tilesAll[ID - (2 * tilesPerRow)]; }
         if (inBounds(Grid.tilesAll, ID + 2) && (ID + 2) % tilesPerRow != 0) { tileRightS = Grid.tilesAll[ID + 2]; }
-        if (inBounds(Grid.tilesAll, ID - 2) && ID % tilesPerRow != 0) { tileLeftS = Grid.tilesAll[ID - 2]; }
+        if (inBounds(Grid.tilesAll, ID - 2) && ID % tilesPerRow != 0) { tileLeftS = Grid.tilesAll[ID - 2]; }*/
 
         if (inBounds(Grid.tilesAll, ID + tilesPerRow + 1) && (ID + tilesPerRow + 1) % tilesPerRow != 0) { tileUpperRight = Grid.tilesAll[ID + tilesPerRow + 1]; }
         if (inBounds(Grid.tilesAll, ID + tilesPerRow - 1) && ID % tilesPerRow != 0) { tileUpperLeft = Grid.tilesAll[ID + tilesPerRow - 1]; }
         if (inBounds(Grid.tilesAll, ID - tilesPerRow + 1) && (ID + 1) % tilesPerRow != 0) { tileLowerRight = Grid.tilesAll[ID - tilesPerRow + 1]; }
         if (inBounds(Grid.tilesAll, ID - tilesPerRow - 1) && ID % tilesPerRow != 0) { tileLowerLeft = Grid.tilesAll[ID - tilesPerRow - 1]; }
         
-        if (inBounds(Grid.tilesAll, ID + tilesPerRow + 2) && (ID + tilesPerRow + 2) % tilesPerRow != 0) { tileUpperRightS = Grid.tilesAll[ID + tilesPerRow + 2]; }
+/*        if (inBounds(Grid.tilesAll, ID + tilesPerRow + 2) && (ID + tilesPerRow + 2) % tilesPerRow != 0) { tileUpperRightS = Grid.tilesAll[ID + tilesPerRow + 2]; }
         if (inBounds(Grid.tilesAll, ID + tilesPerRow - 2) && ID % tilesPerRow != 0) { tileUpperLeftS = Grid.tilesAll[ID + tilesPerRow - 2]; }
         if (inBounds(Grid.tilesAll, ID - tilesPerRow + 2) && (ID + 2) % tilesPerRow != 0) { tileLowerRightS = Grid.tilesAll[ID - tilesPerRow + 2]; }
-        if (inBounds(Grid.tilesAll, ID - tilesPerRow - 2) && ID % tilesPerRow != 0) { tileLowerLeftS = Grid.tilesAll[ID - tilesPerRow - 2]; }
+        if (inBounds(Grid.tilesAll, ID - tilesPerRow - 2) && ID % tilesPerRow != 0) { tileLowerLeftS = Grid.tilesAll[ID - tilesPerRow - 2]; }*/
 
-        if (inBounds(Grid.tilesAll, ID + (2 * tilesPerRow) + 1) && (ID + (2 * tilesPerRow) + 1) % tilesPerRow != 0) { tileUpperRightS1 = Grid.tilesAll[ID + (2 * tilesPerRow) + 1]; }
+/*        if (inBounds(Grid.tilesAll, ID + (2 * tilesPerRow) + 1) && (ID + (2 * tilesPerRow) + 1) % tilesPerRow != 0) { tileUpperRightS1 = Grid.tilesAll[ID + (2 * tilesPerRow) + 1]; }
         if (inBounds(Grid.tilesAll, ID + (2 * tilesPerRow) + 2) && (ID + (2 * tilesPerRow) + 2) % tilesPerRow != 0) { tileUpperRightS2 = Grid.tilesAll[ID + (2 * tilesPerRow) + 2]; }
         if (inBounds(Grid.tilesAll, ID + (2 * tilesPerRow) - 1) && ID % tilesPerRow != 0) { tileUpperLeftS1 = Grid.tilesAll[ID + (2 * tilesPerRow) - 1]; }
         if (inBounds(Grid.tilesAll, ID + (2 * tilesPerRow) - 2) && ID % tilesPerRow != 0) { tileUpperLeftS2 = Grid.tilesAll[ID + (2 * tilesPerRow) - 2]; }
         if (inBounds(Grid.tilesAll, ID - (2 * tilesPerRow) + 1) && (ID + 1) % tilesPerRow != 0) { tileLowerRightS1 = Grid.tilesAll[ID - (2 * tilesPerRow) + 1]; }
         if (inBounds(Grid.tilesAll, ID - (2 * tilesPerRow) + 2) && (ID + 2) % tilesPerRow != 0) { tileLowerRightS2 = Grid.tilesAll[ID - (2 * tilesPerRow) + 2]; }
         if (inBounds(Grid.tilesAll, ID - (2 * tilesPerRow) - 1) && ID % tilesPerRow != 0) { tileLowerLeftS1 = Grid.tilesAll[ID - (2 * tilesPerRow) - 1]; }
-        if (inBounds(Grid.tilesAll, ID - (2 * tilesPerRow) - 2) && ID % tilesPerRow != 0) { tileLowerLeftS2 = Grid.tilesAll[ID - (2 * tilesPerRow) - 2]; }
+        if (inBounds(Grid.tilesAll, ID - (2 * tilesPerRow) - 2) && ID % tilesPerRow != 0) { tileLowerLeftS2 = Grid.tilesAll[ID - (2 * tilesPerRow) - 2]; }*/
 
         if (tileUpper) { adjacentTiles.Add(tileUpper); }
         if (tileLower) { adjacentTiles.Add(tileLower); }
         if (tileLeft) { adjacentTiles.Add(tileLeft); }
         if (tileRight) { adjacentTiles.Add(tileRight); }
         
-        if (tileUpperS) { adjacentTiles.Add(tileUpperS); }
+/*        if (tileUpperS) { adjacentTiles.Add(tileUpperS); }
         if (tileLowerS) { adjacentTiles.Add(tileLowerS); }
         if (tileLeftS) { adjacentTiles.Add(tileLeftS); }
-        if (tileRightS) { adjacentTiles.Add(tileRightS); }
+        if (tileRightS) { adjacentTiles.Add(tileRightS); }*/
 
         if (tileUpperLeft) { adjacentTiles.Add(tileUpperLeft); }
         if (tileUpperRight) { adjacentTiles.Add(tileUpperRight); }
         if (tileLowerLeft) { adjacentTiles.Add(tileLowerLeft); }
         if (tileLowerRight) { adjacentTiles.Add(tileLowerRight); }
         
-        if (tileUpperLeftS) { adjacentTiles.Add(tileUpperLeftS); }
+/*        if (tileUpperLeftS) { adjacentTiles.Add(tileUpperLeftS); }
         if (tileUpperRightS) { adjacentTiles.Add(tileUpperRightS); }
         if (tileLowerLeftS) { adjacentTiles.Add(tileLowerLeftS); }
-        if (tileLowerRightS) { adjacentTiles.Add(tileLowerRightS); }
+        if (tileLowerRightS) { adjacentTiles.Add(tileLowerRightS); }*/
         
-        if (tileUpperLeftS1) { adjacentTiles.Add(tileUpperLeftS1); }
+/*        if (tileUpperLeftS1) { adjacentTiles.Add(tileUpperLeftS1); }
         if (tileUpperLeftS2) { adjacentTiles.Add(tileUpperLeftS2); }
         if (tileUpperRightS1) { adjacentTiles.Add(tileUpperRightS1); }
         if (tileUpperRightS2) { adjacentTiles.Add(tileUpperRightS2); }
         if (tileLowerLeftS1) { adjacentTiles.Add(tileLowerLeftS1); }
         if (tileLowerLeftS2) { adjacentTiles.Add(tileLowerLeftS2); }
         if (tileLowerRightS1) { adjacentTiles.Add(tileLowerRightS1); }
-        if (tileLowerRightS2) { adjacentTiles.Add(tileLowerRightS2); }
+        if (tileLowerRightS2) { adjacentTiles.Add(tileLowerRightS2); }*/
 
         CountTarget();
     }
